043473dac85f155f4d86a2fe9325992bfa2a2477
Vijay <rebalvijay930@gmail.com>
2025-07-02 11:07:52 +0530

    Created comment model with refPath

---
+++ src/models/comment.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet','Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    }
+},{timestamps: true});
+
+const Comment = mongoose.model('Comment',commentSchema);
+export default Comment;
